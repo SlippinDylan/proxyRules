04ef4e5c66533b29f84fa843d332565626bc9491
Dylan Wang <dylanwang@MacBook-Pro.local>
2025-03-26 15:50:03 +0800

    更新规则

--- backup/Conversion.sh
+++ backup/Conversion.sh
@@ -14,3 +14,27 @@
 mihomo convert-ruleset domain text XXX.yaml XXX.mrs
 
 mihomo convert-ruleset ipcidr text XXX.yaml XXX.mrs
+
+mihomo convert-ruleset domain text Direct.txt Direct.mrs
+mihomo convert-ruleset domain text Proxy-Develop.txt Proxy-Develop.mrs
+mihomo convert-ruleset domain text Proxy-Download.txt Proxy-Download.mrs
+mihomo convert-ruleset domain text Proxy-Intelligence.txt Proxy-Intelligence.mrs
+mihomo convert-ruleset domain text Proxy-Media.txt Proxy-Media.mrs
+mihomo convert-ruleset domain text Proxy-Network.txt Proxy-Network.mrs
+mihomo convert-ruleset domain text Proxy-Tools.txt Proxy-Tools.mrs
+
+
+mihomo convert-ruleset ipcidr text Direct-IP.txt Direct-IP.mrs
+mihomo convert-ruleset ipcidr text Proxy-Develop-IP.txt Proxy-Develop-IP.mrs
+mihomo convert-ruleset ipcidr text Proxy-Tools-IP.txt Proxy-Tools-IP.mrs
+
+
+
+mihomo convert-ruleset domain text Direct.txt Direct.mrs
+mihomo convert-ruleset domain text Proxy-Develop.txt Proxy-Develop.mrs
+mihomo convert-ruleset domain text Proxy-Intelligence.txt Proxy-Intelligence.mrs
+mihomo convert-ruleset domain text Proxy-Media.txt Proxy-Media.mrs
+mihomo convert-ruleset domain text Proxy.txt Proxy.mrs
+mihomo convert-ruleset ipcidr text Direct-IP.txt Direct-IP.mrs
+mihomo convert-ruleset ipcidr text Proxy-Develop-IP.txt Proxy-Develop-IP.mrs
+mihomo convert-ruleset ipcidr text Proxy-IP.txt Proxy-IP.mrs
